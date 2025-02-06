f16bc87f8de09e5ca08131d0603ebe711b884305
HadiFawwaz <hadifawwazsudewo@gmail.com>
2025-02-06 11:34:33 +0700

    php baru (lagi)

---
+++ HitungPersegiPanjang.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "OPERATOR ARITMATIKA <br>";
+echo "<hr>";
+
+
+$Panjang = 20;
+$Lebar = 4;
+$cm = "cm";
+
+$luas = $Panjang * $Lebar;
+$keliling = 2 * ($Panjang + $Lebar);
+
+echo "Luas Persegi Panjang : $Panjang + $Lebar = ". $luas . " " .$cm . "<br>";
+echo "Keliling Persegi Panjang: 2 x ($Panjang + $Lebar) = ". $keliling . " " . $cm . "<br>";
