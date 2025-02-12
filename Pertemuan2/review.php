f732743f87d5ab44861fb76914bad0f1b5859e3b
HadiFawwaz <hadifawwazsudewo@gmail.com>
2025-02-12 09:25:38 +0700

    review

---
+++ Pertemuan2/review.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$nama_lengkap = "Rais Bima Prayata";
+$usia = "15 tahun";
+$alamat = "Sukaraja";
+$anak_ke = 3;
+
+echo "<br>------ Data Diri ------ <br>";
+echo "Nama lengkap : $nama_lengkap <br>";
+echo "Usia : $usia <br>";
+echo "Alamat : $alamat <br>";
+echo "Anak Ke : $anak_ke <br>";
